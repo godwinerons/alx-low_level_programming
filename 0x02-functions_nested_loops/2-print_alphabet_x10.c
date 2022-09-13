30358c6553c2ebbc61b711dfd950c9de2f7297ea
eronsmahon <godwinerons89@gmail.com>
2022-09-13 19:25:45 +0100

    nested_loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+*/
+void print_alphabet_x10(void);
+{
+	int r = 0;
+	char l = 'a'
+
+		while(r < 0)
+		{
+			l = 'a';
+			while(l <= 'z')
+			{
+				putchar(l);
+				l++;
+			}
+			putchar('\n');
+
+			r++;
+		}
+
+}
