0c576a29622be883bcab71349b144b7ed5ada85b
eronsmahon <godwinerons89@gmail.com>
2022-09-13 20:29:13 +0100

    nasted loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: 0;
+ */
+void print_alphabet(void)
+{
+	char l = 'a';
+	while (l<= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
