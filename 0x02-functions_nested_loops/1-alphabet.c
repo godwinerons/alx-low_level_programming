a9cf4b6318bd220ed83c01cdee0143f6c72c64bb
eronsmahon <godwinerons89@gmail.com>
2022-09-13 14:46:09 +0100

    nested_loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main"
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: 0;
+ */
+void print_alphabet(void)
+{
+	char l = 'a';
+	while (l<= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+
