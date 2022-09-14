4f1822d59dd35363ab1f18582ea581a3a6cb6846
eronsmahon <godwinerons89@gmail.com>
2022-09-14 15:44:01 +0100

    nested_loop

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * main - print the sum of the multuple of 3 and 5 below 1024
+ * Return: 1 (if letter)
+ */
+int main(void)
+{
+	int c;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+		c++;
+
+	}
+	return (0);
+}
