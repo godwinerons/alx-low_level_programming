36cc4f99b33fc987a3680c476421b202c41e68a4
eronsmahon <godwinerons89@gmail.com>
2022-09-19 14:13:37 +0100

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int str = 0;
+
+	while (str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	_putchar('\n')
+
+}
