baa15691d642c2e42f8de621cbd0a2d5115ab90d
eronsmahon <godwinerons89@gmail.com>
2022-09-22 09:56:57 +0100

    more pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,12 @@ char *string_toupper(char *s)
 {
 	int a = 0;
 
-	while (*(s + count) != '\0')
+	while (*(s + a) != '\0')
 	{
 		if ((*(s + a) >= 97) && (*(s + a) <= 122))
 			*(s + a) = *(s + a) - 32;
 		a++;
 	}
+
 	return (s);
 }
