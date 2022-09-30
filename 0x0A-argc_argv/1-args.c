fe71abe540301aff9d9329064ae5f23de60f9d26
eronsmahon <godwinerons89@gmail.com>
2022-09-30 13:10:01 +0100

    argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print all argument 
+ * main - print the number of argument passed into it
  * @argc: number of command line argument
  * @argv: array that contains the program command line argument
  * Return: 0 -success
